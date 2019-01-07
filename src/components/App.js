e7551654e9bae9767dfd0ee3ca337a013cd54236
ndb796 <ndb796@naver.com>
2019-01-08 06:09:58 +0900

    Initialize Project & Make Navigation Bar

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppShell from './AppShell';
+
+class App extends React.Component {
+    render() {
+        return (
+            <AppShell>
+                <h1>Hello World!</h1>
+            </AppShell>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/AppShell.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
+import { AppBar, Drawer, MenuItem } from 'material-ui';
+
+class AppShell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
+    handleDrawerToggle = open => {
+        this.setState({open: !this.state.open})
+    }
+    handleRequestChange = open => {
+        this.setState({open: open})
+    }
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <AppBar onClick={this.handleDrawerToggle}/>
+                    <Drawer open={this.state.open} docked={false}
+                     onRequestChange={this.handleRequestChange}>
+                        <MenuItem primaryText={'Home'}/>
+                    </Drawer>
+                    <div id="content">
+                        {React.cloneElement(this.props.children)}
+                    </div>
+                </div>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+export default AppShell;
